837a2be88c7fa14ea63d073df34fd5a50c58c0cb
chaosfissure <samhimes@gmail.com>
2013-12-18 17:53:40 -0500

    Orbit functionality and non-intersecting collision avoidance...

--- js/collision.js
+++ js/collision.js
@@ -1,10 +1,12 @@
-function getPlanetForces(shipPosition) {
+function orbitRadiusCheck(shipPosition) {
 
 	//As of yet, all we need to do with collisions is
 	//avoid the sun and avoid planets.
-	
-	var totalForce = new THREE.Vector3(0,0,0);
+
 	var origin = new THREE.Vector3(0,0,0);
+	
+	//How much distance away from said object do we want?
+	var radiusMultiplier = 1.5;
 	
 	//----- Collisions against the Sun -----
 	//True statement for scoping variables...
@@ -12,71 +14,89 @@ function getPlanetForces(shipPosition) {
 		var sun = stars[CURRENT_LOCATION];
 		var sunPos = origin.clone();
 		var sunDist = sunPos.clone().sub(shipPosition);
-		var reverse = shipPosition.clone().sub(sunPos);
-		if (sunDist.length() < sun.radius*2) {
-			//Force the thing away from the sun.
-			var normal = sunDist.clone().normalize();
-			var angle = (normal.dot(shipPhys.velocity))/(normal.length()*shipPhys.velocity.length());
-			
-			var deflect = normal.clone().multiplyScalar((shipPhys.velocity.dot(normal))/(normal.dot(normal)));
-			
-			var proj = shipPhys.velocity.clone().sub(deflect);
-			var newnormal = (proj.sub(deflect));
-			
-			//If the current acceleration isn't going to cause
-			//the ship to crash into the object, don't adjust
-			//its flight path.
-			
-			if (shipPhys.acceleration.angleTo(newnormal) > Math.PI/4) { 
-				totalForce.add(newnormal.multiplyScalar(0.75));
+		//check and see if the ship actually is heading towards the sun...
+		var positionWithAcceleration = shipPosition.clone().add(shipPhys.acceleration);
+		var trajectoryDist = sunPos.clone().sub(positionWithAcceleration);
+		if (sunDist.length() < sun.radius*radiusMultiplier) {
+			if (trajectoryDist.length() >= sun.radius*radiusMultiplier) {
+				//shipPhys.velocity.multiplyScalar(0.0);
+				orbiting[0] = "none";
 			}
 			else {
-				totalForce.add(reverse).normalize();
-			}	
+				orbiting[0] = "sun";
+				shipPhys.velocity.multiplyScalar(0.975);
+				
+				var angle = (new THREE.Vector3(1,0,0)).angleTo(shipPosition.clone().normalize());
+				var angleDegrees = toDegrees(angle);
+				orbiting[1] = sun.position.clone();
+				
+				if (angleDegrees >=0 && angleDegrees <= 180) { orbiting[2] = true; }
+				else { orbiting[2] = false; }
+				
+				if ("sun" === goalObject) { travelling = false; }
+			}
 		}
-		
-		while (shipPosition.length() < sun.radius+10) { 
-			shipPosition.add(newnormal);
-		}
-		
 	}
 	
 	//----- Collisions against Planets -----
-	var sysplanets = stars[CURRENT_LOCATION].planets;
 	var i = 0;
 	while (scenePlanets["planet"+i] !== undefined)
 	{
 		var planet = scenePlanets["planet"+i];
 		var planetExtended = grabObject("planet"+i)[1];
 		var newShipPosition = planetExtended.position.clone().sub(shipPosition);
-		//Normalize planet to be the center of the system
+		//check and see if the ship actually is heading towards the planet...
+		var positionWithAcceleration = newShipPosition.clone().add(shipPhys.acceleration);
+		var trajectoryDist = planetExtended.position.clone().sub(positionWithAcceleration);
 		var planetDist = origin.clone().sub(newShipPosition);
-		var reverse = newShipPosition.clone().sub(origin);
-		if (planetDist.length() < planet.radius*2) {
-			if (goalObject == ("planet"+i)) {
-			shipPhys.velocity.multiplyScalar(0.825);
-			}
-			//Force the thing away from the planet.
-			var normal = planetDist.clone().normalize();
-			var angle = (normal.dot(shipPhys.velocity))/(normal.length()*shipPhys.velocity.length());
-			
-			var deflect = normal.clone().multiplyScalar((shipPhys.velocity.dot(normal))/(normal.dot(normal)));
-			
-			var proj = shipPhys.velocity.clone().sub(deflect);
-			var newnormal = (proj.sub(deflect));
-			
-			if (shipPhys.acceleration.angleTo(newnormal) > Math.PI/4) { 
-				totalForce.add(newnormal.multiplyScalar(0.75));
+		
+		
+		if (("planet"+i) === goalObject && planetDist.length() < planet.radius*radiusMultiplier) {
+			orbiting[0] = "planet"+i;
+			orbiting[1] = planet.position.clone();
+		
+		}
+		
+		else if (planetDist.length() < planet.radius*radiusMultiplier) {
+			if (trajectoryDist.length() >= planet.radius*radiusMultiplier) {
+				shipPhys.velocity.multiplyScalar(0.0);
+				orbiting[0] = "none";
 			}
 			else {
-				totalForce.add(reverse).normalize();
-			}	
+				orbiting[0] = "planet"+i;
+				shipPhys.velocity.multiplyScalar(0.975);
+			
+				orbiting[1] = planet.position.clone();
+				
+				if (angleDegrees >=0 && angleDegrees <= 180) { orbiting[2] = true; }
+				else { orbiting[2] = false; }
+				
+				if ("planet"+i === goalObject) { travelling = false; }
+			}
 		}
-		while (newShipPosition.length() < planet.radius+10) { 
-			shipPosition.add(newnormal.normalize());
-			newShipPosition.add(newnormal.normalize());
-		}
+
+		//Hey I'm a moron for not incrementing loop variables and wondering why stuff didn't work!
 		i++;
 	}
-	return totalForce;
+}
+
+function doOrbitUpdate(ship) {
+
+	var oldPosition = ship.position.clone();
+	var orbitAmount = 0.0025;
+	var newShipPosition = ship.position.clone().sub(orbiting[1]);
+	
+	if (orbiting[2]) {
+		newShipPosition.applyEuler(new THREE.Euler(0,-orbitAmount,0,'XYZ'));
+	}
+	else {
+		newShipPosition.applyEuler(new THREE.Euler(0,orbitAmount,0,'XYZ'));
+	}
+	
+
+	newShipPosition.add(orbiting[1]);
+	ship.position = newShipPosition.clone();
+	
+	return (newShipPosition.clone().sub(oldPosition)).add(newShipPosition);
+	
 }
