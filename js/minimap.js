d40f4b72b1a53f98be2f7e604bb7f6b09ce5c6c5
Alfie Parthum <brpxqzme+github@gmail.com>
2013-12-18 20:15:06 -0500

    I thought this went up...

---
+++ js/minimap.js
@@ -0,0 +1,22 @@
+// Map camera, map scene
+var mapcam, mapscn;
+// Star particle geometry
+var stars_system;
+
+function makeMap () {
+    mapscn = new THREE.Scene();
+    mapcam = new THREE.OrthographicCamera(-50000, 50000,
+                                          -50000, 50000,
+                                          -6000, 6000);
+    var stars_geometry = new THREE.Geometry();
+    for (var i in stars)
+        stars_geometry.vertices.push(new THREE.Vector3(stars[i].x,
+                                                       stars[i].y,
+                                                       stars[i].z));
+    var stars_partmat = new THREE.ParticleBasicMaterial({ color: 0xffffff,
+                                                          size: 1 });
+    var stars_system = new THREE.ParticleSystem(stars_geometry,
+                                                stars_partmat);
+    mapscn.add(stars_system);
+    mapscn.add(mapcam);
+}
